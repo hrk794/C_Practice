01d3da12e98175f05c397b8141e93f9acc3c900a
hrk794 <hrk794@gmail.com>
2019-08-31 15:35:22 +0530

    Getting data into the struct variable from the user

--- struct.c
+++ struct.c
@@ -14,9 +14,28 @@ int main()
     // Creating a struct variable
     struct student s1 = {"Bob", 3, 20};
     
-    // Method of accessing the variable         
+    
+    
+    // Dynamically doing the same thing
+    
+    struct student s2;
+    
+    printf("Please enter your name: ");
+    scanf("%s", s2.name);
+    printf("Please enter your Roll number: ");
+    scanf("%d", &s2.roll);
+    printf("Please enter your age: ");
+    scanf("%d", &s2.age);
+    
+    // Method of accessing the variable     
     printf("Name: %s \n", s1.name);
     printf("Roll number: %d \n", s1.roll);
     printf("Age: %d \n", s1.age);
+    
+    printf("\n");
+    
+    printf("Name: %s \n", s2.name);
+    printf("Roll number: %d \n", s2.roll);
+    printf("Age: %d \n", s2.age);
 
 }
