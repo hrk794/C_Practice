b6510cd111a98528c43c855e40cf5386dbb0af6c
hrk794 <hrk794@gmail.com>
2019-09-07 21:46:29 +0530

    Exercise 12-10

---
+++ Dan_Gookin_C_Dummies/e12-9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	char firstname[16];
+	
+	printf("What is your name? ");
+	fgets(firstname,16,stdin);
+	printf("Pleased to meet you, %s\n", firstname);
+
+	return 0;
+}
