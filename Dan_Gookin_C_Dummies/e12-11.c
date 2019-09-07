1ad9d04b23cfd86e5c5d58685d09f21bbed4dea2
hrk794 <hrk794@gmail.com>
2019-09-07 22:06:17 +0530

    Exercise 12-11

---
+++ Dan_Gookin_C_Dummies/e12-11.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	char firstname[16];
+	char lastname[16];
+	
+	printf("What is your first name? ");
+	scanf("%s", firstname);
+	printf("What is your last name? ");
+	scanf("%s", lastname);
+	printf("Pleased to meet you, %s %s\n", firstname, lastname);
+
+	return 0;
+}
