a6c7e15bae6cee38c4b8b5d7d98c3970d9822f3b
hrk794 <hrk794@gmail.com>
2019-09-12 22:01:47 +0530

    Exercise 12-15

---
+++ Dan_Gookin_C_Dummies/e12-15.c
@@ -0,0 +1,75 @@
+/*  Using the source code from Listing 12-5 as a starting point,
+create a program that generates 40 random numbers in the range from 1
+through 100 and stores those values in an array. Display that array. Sort that
+array. Display the results.
+
+*/
+
+
+
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+	int size = 40;
+	int highscore[size];
+	srand((unsigned)time(NULL));
+	int x, a, b, temp, inner, outer;
+
+	for (x = 0; x < size; x++)
+	{
+		highscore[x] = (rand() % 100) + 1;
+	}
+
+	// Display the array
+	x = 0;
+	printf("Here are your scores:\n");
+	for (a = 0; a < 4; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			printf("%d\t", highscore[x]);
+			x++;
+		}
+		printf("\n");
+	}
+	
+
+
+	// Sort the array
+	for (outer = 0; outer < (size-1); outer++)
+	{
+		for (inner=outer+1; inner < size; inner++)
+		{
+			if (highscore[outer] < highscore[inner])
+				{
+					temp = highscore[outer];
+					highscore[outer] = highscore[inner];
+					highscore[inner] = temp;
+				}
+		}
+	}
+	
+
+
+	// Display the sorted array
+	x = 0;
+	printf("Here are your scores:\n");
+	for (a = 0; a < 4; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			printf("%d\t", highscore[x]);
+			x++;
+		}
+		printf("\n");
+	}
+
+
+	return 0;
+}
