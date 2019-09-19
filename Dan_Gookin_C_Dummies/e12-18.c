d68445c749daa00f0d1a504783e6cd03acaeef95
hrk794 <hrk794@gmail.com>
2019-09-19 22:48:57 +0530

    Exercise 12-18

---
+++ Dan_Gookin_C_Dummies/e12-18.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define SIZE 4
+
+int main()
+{
+		char president[SIZE][8] = {
+			"Clinton",
+			"Bush",
+			"Obama",
+			"Trump"
+		};
+		int index,x;
+
+		for (x = 0; x < SIZE; x++)
+		{
+			index = 0;
+			while (president[x][index]  != '\0')
+			{
+				putchar(president[x][index]);
+				index++;
+			}
+			putchar('\n');
+		}
+		
+
+	return 0;
+}
