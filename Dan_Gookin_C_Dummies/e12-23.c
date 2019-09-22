e304241295672b2678b4ef7f1918974a9754bbfc
hrk794 <hrk794@gmail.com>
2019-09-22 11:45:51 +0530

    Exercise 12-23

---
+++ Dan_Gookin_C_Dummies/e12-23.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define SIZE 3
+
+void showarray(int array[]);
+
+int main()
+{
+	int n[] = { 1, 2, 3, 4, 5, 7 };
+
+	puts("Here's your array: ");
+	showarray(n);
+
+	return 0;
+
+}
+
+
+void showarray(int array[])
+{
+	int x;
+
+	for(x=0;x<SIZE;x++)
+		printf("%d\t", array[x]);
+	putchar('\n');
+}
