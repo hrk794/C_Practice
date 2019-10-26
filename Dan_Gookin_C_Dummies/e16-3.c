7e94f67d77168686a8434e52f80a550a10d4ddb1
hrk794 <hrk794@gmail.com>
2019-10-26 23:21:36 +0530

    Exercise 16-3

---
+++ Dan_Gookin_C_Dummies/e16-3.c
@@ -0,0 +1,19 @@
+/*
+Introducing typedef
+*/
+
+
+
+#include <stdio.h>
+
+typedef int stinky;
+
+stinky main()
+{
+	stinky a = 2;
+	
+	printf("Everyone knows that ");
+	printf("%d + %d = %d\n", a, a, a+a);
+
+	return 0;
+}
